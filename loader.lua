055d4e162f85590dae3072d693b7aab00c137280
PixelatedColors <95421998+PixelatedColors@users.noreply.github.com>
2026-01-02 20:14:05 +0530

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,48 @@
+print("/ / / / / / / / HYDROX REFRESHED / / / / / / / /")
+print("Thank you for using Hydrox REFRESHED")
+wait(1)
+print("VERSION CHECKER LOADING....")
+wait(1)
+print("VERSION - A.1.1.26 (LATEST)")
+wait(1)
+print("Installing Dependencies (takes longer on different executors)")
+wait(2)
+print("Loading UILibrary")
+print("Loaded!")
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "🎆 Hydrox - REFRESHED | Key System (K5/1/26)",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "key.sys",
+   ShowText = "Hydrox", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "hydrox"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Hydrox REFRESHED | Key System",
+      Subtitle = "Key System",
+      Note = "Open the link in your browser.", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/Pth0rxbW"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
